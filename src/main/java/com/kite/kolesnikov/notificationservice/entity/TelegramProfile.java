a8556f78b5e4cfdbbc5afca2dfb98a02f635b2c1
Alexander Kolesnikov <125891795+AlexanderKolesnikow@users.noreply.github.com>
2023-10-24 14:03:16 +0100

    Entity and repository

---
+++ src/main/java/com/kite/kolesnikov/notificationservice/entity/TelegramProfile.java
@@ -0,0 +1,36 @@
+package com.kite.kolesnikov.notificationservice.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "telegram_profile")
+public class TelegramProfile {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "user_name")
+    private String userName;
+
+    @Column(name = "user_id")
+    private Long userId;
+
+    @Column(name = "chat_id")
+    private Long chatId;
+}
